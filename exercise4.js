ec8a6ae056cd2bb432bb173337c7eed581a71b8d
Scarlet Salinas <2022156127@ub.edu.bz>
2026-02-16 01:24:07 -0600

    Complete Exercise 4: Identify the bug

--- exercise4.js
+++ exercise4.js
@@ -1,8 +1,8 @@
 const box = document.querySelector('.box');
 box.addEventListener('click', (event) => {
-    // BUG: this.classList.add('active') throws an error
-this.classList.add('active');
+    event.currentTarget.classList.add('active');
 });
 
 // Arrow functions do not have their own this. 
 // this refers to global window oject, not the box element, and Window.classList doesn't exist, so the code throws and error.
+//event.currentTarget always refers to the element the listener is attached to, in this case, that's .box
